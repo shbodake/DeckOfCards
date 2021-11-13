87e3a367a48d5ad6dd7d990f9187c4351264b94f
shbodake <sayalibodake28@gmail.com>
2021-11-13 20:57:01 +0530

    Added: Receive the card and further order to play

--- src/com/blp/deckofcards/Player.java
+++ src/com/blp/deckofcards/Player.java
@@ -1,4 +1,20 @@
 package com.blp.deckofcards;
 
+import java.util.ArrayList;
+
 public class Player {
+    String name;
+    ArrayList<String> cardList = new ArrayList<>();
+
+    public Player(String name) {
+        this.name = name;
+    }
+    public void setCardList(String card) {
+        cardList.add(card);
+    }
+    public void getCardList() {
+        for (String position : cardList) {
+            System.out.print(position + ", ");
+        }
+    }
 }
